357caf0e39822a7912c78e72b5749a198060bff1
Galip Efe Öncü <galipefe75@gmail.com>
2025-07-05 23:34:31 +0300

    procedural landmass generator beginning

---
+++ SurvivalGame/Assets/Scripts/Editor/MapGeneratorEditor.cs
@@ -0,0 +1,27 @@
+using UnityEditor;
+using UnityEngine;
+
+// Beyin 
+
+[CustomEditor(typeof(MapGenerator))]
+
+public class MapGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        MapGenerator mapGenerator = (MapGenerator)target;
+
+        if (DrawDefaultInspector())
+        {
+            if (mapGenerator.autoUpdate)
+            {
+                mapGenerator.GenerateMap();
+            }
+        }
+
+        if (GUILayout.Button("Generate"))
+        {
+            mapGenerator.GenerateMap();
+        }
+    }
+}
---
+++ SurvivalGame/Assets/Scripts/NoiseMapGenerator/MapGenerator.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+
+
+public class MapGenerator : MonoBehaviour
+{
+    public int mapWidth;
+    public int mapHeight;
+    public float noiseScale;
+
+    public bool autoUpdate;
+
+    public void GenerateMap()
+    {
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+
+        MapDisplay display = Object.FindFirstObjectByType<MapDisplay>();
+        display.DrawNoiseMap(noiseMap);
+    }
+}
