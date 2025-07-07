1eeea143f7bf46bb4354e0922ce2df3fce8ee584
Galip Efe Öncü <galipefe75@gmail.com>
2025-07-07 12:45:13 +0300

    Mesh Renderer

--- SurvivalGame/Assets/Scripts/NoiseMapGenerator/TextureGenerator.cs
+++ SurvivalGame/Assets/Scripts/NoiseMapGenerator/TextureGenerator.cs
@@ -6,6 +6,8 @@ public static class TextureGenerator
     {
         Texture2D texture = new Texture2D(width, height);
         texture.SetPixels(colorMap);
+        texture.filterMode = FilterMode.Point;
+        texture.wrapMode = TextureWrapMode.Clamp;
         texture.Apply();
         return texture;
     }
