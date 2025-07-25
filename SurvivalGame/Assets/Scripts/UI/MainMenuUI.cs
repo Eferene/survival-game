efa675ec5b95f6d6a327bf2e5360a725ab75700a
Galip Efe Öncü <galipefe75@gmail.com>
2025-07-25 11:34:53 +0300

    Add new scenes and loading manager for main menu
    Replaced TestScene with GameScene, and added HomeScene and LoadingScene to the project. Introduced LoadingManager script to handle scene loading with a progress bar, and added a placeholder MainMenuUI script. Updated build settings and font asset to support new UI elements.

---
+++ SurvivalGame/Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour
+{
+    //public void LoadMap()
+    //{
+    //    LoadingManager.sceneToLoad = "GameScene"; // Yüklenecek sahne ismi
+
+    //    SceneManager.LoadScene("LoadingScene"); // Yükleme sahnesine geçiş yap
+    //}
+}
