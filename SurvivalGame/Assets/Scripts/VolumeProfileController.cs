7acda66f6f3779c79d58bf4d18194d0530a2c3db
Galip Efe Öncü <galipefe75@gmail.com>
2025-08-13 22:09:56 +0300

    Add underwater and surface volume profiles with controller

---
+++ SurvivalGame/Assets/Scripts/VolumeProfileController.cs
@@ -0,0 +1,47 @@
+﻿using Unity.Cinemachine;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class VolumeProfileController : MonoBehaviour
+{
+    [Header("Depth Parameters")]
+    [SerializeField] private CinemachineCamera _camera;
+    [SerializeField] private float _depth;
+    [SerializeField] private float _fogDensity;
+
+    [Header("Post Processing Volume")]
+    [SerializeField] private Volume _postProcessingVolume;
+
+    [Header("Post Processing Profiles")]
+    [SerializeField] private VolumeProfile _surfaceVolumeProfile;
+    [SerializeField] private VolumeProfile _underwaterVolumeProfile;
+
+
+    private void Start()
+    {
+        RenderSettings.fogDensity = _fogDensity / 10;
+        RenderSettings.fogColor = Color.cyan;
+    }
+
+    private void Update()
+    {
+        if (_camera.transform.position.y < _depth)
+            EnableEffect(true);
+        else
+            EnableEffect(false);
+    }
+
+    private void EnableEffect(bool enable)
+    {
+        if (!enable)
+        {
+            RenderSettings.fog = false;
+            _postProcessingVolume.profile = _surfaceVolumeProfile;
+        }
+        else
+        {
+            RenderSettings.fog = true;
+            _postProcessingVolume.profile = _underwaterVolumeProfile;
+        }
+    }
+}
