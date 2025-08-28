b3b397b08e7d2fa1ed49ee634c478f88e8c11673
Galip Efe Öncü <galipefe75@gmail.com>
2025-08-28 12:23:37 +0300

    Add NavMeshGenerator script and update NavMesh assets

---
+++ SurvivalGame/Assets/Scripts/NavMeshGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using Unity.AI.Navigation; // Bu using önemli
+using UnityEngine;
+
+public class NavMeshGenerator : MonoBehaviour
+{
+    [SerializeField] private NavMeshSurface mainIsland;
+
+    // Bu metodu event'ten çağıracağız.
+    public void GenerateNavMeshAsync()
+    {
+        StartCoroutine(UpdateNavMeshCoroutine());
+    }
+
+    private IEnumerator UpdateNavMeshCoroutine()
+    {
+        Debug.Log("Asenkron NavMesh bake işlemi başlıyor, chill bro...");
+
+        // İşte sihirli kısım bu. UpdateNavMesh asenkron çalışır.
+        AsyncOperation operation = mainIsland.UpdateNavMesh(mainIsland.navMeshData);
+
+        // Bake işlemi bitene kadar bekle.
+        while (!operation.isDone)
+        {
+            // İstersen burada operation.progress ile bir loading bar bile doldurabilirsin.
+            yield return null;
+        }
+        Debug.Log("NavMesh Bake tamamdır! Agents are good to go.");
+    }
+}
